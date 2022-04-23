91db2c8f256cad31f5a1945590b65fa9371dd712
Jack Tiebie <jacktiebie45@gmail.com>
2022-04-23 13:35:39 +0200

    code

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@ import React from "react"
 
 export default function App() {
     const [count, setCount] = React.useState(0)
-    /**
-     * Note: if you ever need the old value of state
-     * to help you determine the new value of state,
-     * you should pass a callback function to your
-     * state setter function instead of using
-     * state directly. This callback function will
-     * receive the old value of state as its parameter,
-     * which you can then use to determine your new
-     * value of state.
-     */
+   
     function add() {
         setCount(prevCount => prevCount + 1)
     }
